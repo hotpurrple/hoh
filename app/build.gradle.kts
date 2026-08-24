3eaeaa0b9797a3beba755c9fc8e0526405e5d637
hotpurrple <112518799+hotpurrple@users.noreply.github.com>
2026-08-25 01:00:58 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.koreadervoicepager"
+        applicationId = "com.koreadervoicepager.cld"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
